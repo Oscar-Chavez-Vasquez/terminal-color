5429ae1da3028e6eb5ba42691dabdbb4e8f096ae
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2025-11-05 10:35:36 -0300

    feat: agrega funciones de mensajes con chalk

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importamos la librería chalk
+import chalk from 'chalk';
+
+// Mensajes iniciales
+console.log(chalk.blue('👋 ¡Bienvenido a la Terminal Parlante!'));
+console.info(chalk.green('ℹ️ Esta terminal está viva... y tiene estilo.'));
+console.warn(chalk.yellow('⚠️ Atención: Esto puede ponerse colorido.'));
+console.error(chalk.red('❌ Error simulado: algo salió demasiado bien.'));
+console.log(chalk.magenta('💡 Recuerda: la práctica hace al programador.'));
+
+// BONUS: función básica
+function saludar(nombre) {
+  console.log(chalk.cyan(`Hola ${nombre}, ¡bienvenido al mundo JavaScript! 🌍`));
+}
+
+// Prueba de función
+saludar("Equipo JS");
+
+
+// 
+function mostrarEstado() {
+    console.log(chalk.bgGreen('🎉 ¡El programa ha iniciado exitosamente! 🎉'));
+    console.info(chalk.bgBlue('🔧 El programa se está ejecutando 🔧'));
+    console.info(chalk.bgYellowBright('🎇 El programa finalizó exitosamente 🎇'));
+  return;
+}
+mostrarEstado()
